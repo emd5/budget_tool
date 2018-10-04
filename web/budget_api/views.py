b1a177b9c6e50bab36e013a825c0de283bad7fac
emd5 <emahoney5@mail.greenriver.edu>
2018-10-04 08:45:04 -0700

    update rest api, refactored file names

--- web/budget_api/views.py
+++ web/budget_api/views.py
@@ -4,14 +4,17 @@ from .serializers import UserSerializer, User
 
 
 class RegisterApiView(generics.CreateAPIView):
+    """Concrete view for creating a model instance to serialize user object """
     permission_classes = ''
     authentication_classes = (TokenAuthentication,)
     serializer_class = UserSerializer
 
 
 class UserApiView(generics.RetrieveAPIView):
+    """Concrete view for creating a model instance to specific user object by id"""
     permission_classes = ''
     serializer_class = UserSerializer
 
     def get_queryset(self):
         return User.objects.filter(id=self.kwargs['pk'])
+
