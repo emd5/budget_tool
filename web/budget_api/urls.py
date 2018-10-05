9c99db8a3fe1c31f8442ea93118dfd89c38f6893
emd5 <emahoney5@mail.greenriver.edu>
2018-10-04 22:09:36 -0700

    add serializer classes for budget and transaction endpoints, issues with relative path import modules

--- web/budget_api/urls.py
+++ web/budget_api/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import RegisterApiView, UserApiView
+from .views import RegisterApiView, UserApiView, TransactionListApiView, BudgetListApiView
 from rest_framework.authtoken import views
 
 urlpatterns = [
     path('user/<int:pk>', UserApiView.as_view(), name='user_detail'),
     path('register', RegisterApiView.as_view(), name='register'),
     path('login', views.obtain_auth_token),
-    # path('budget', BudgetListApiView.as_view(), name='budget_view'),
+    path('budget', BudgetListApiView.as_view(), name='budget_view'),
     # path('budget/new/', BudgetCreateApiView.as_view(), name='budget_create'),
     # path('transaction/<int:id>', TransactionDetailApiView.as_view(), name='transaction_detail'),
     # path('transaction/new', TransactionCreateApiView.as_view(), name="transaction_create")
